fa0b9e9f925eb3a8387dc151c9b246f6636b0242
Shunya Nagashima <syun864297531@gmail.com>
2022-01-04 12:47:13 +0900

    forms.pyを追加し、home.htmlとwelcome.htmlを移動させました

---
+++ accounts/forms.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class SignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model   = CustomUser
+        
